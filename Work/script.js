9ed332dbcd1056190af1234927b8f679426d1a7e
Ronyx <valnos04@gmail.com>
2021-03-29 23:53:41 +0300

    commit

---
+++ Work/script.js
@@ -0,0 +1,78 @@
+// let object = {
+//     auto: false,
+//     uplayCode: 3245,
+//     Name: "Ronix",
+// };
+// console.log (object.Name);
+
+// let OBject = ["Ronix", 23453, false, "orange.png"]
+// console.log (OBject[3])
+
+// const answer = +prompt("Do you have 18?","18");
+// console.log(answer + 10);
+
+// let answers = [];
+// answers[1] = prompt("How are you?", "");
+// answers[2] = prompt("How old are you?", "");
+// document.write(answers);
+// console.log(answers[0]);
+// let user = "valera";
+// alert(`Hello, ${user}`);
+
+
+// console.log (121 === "60");
+
+// let decl = 11;
+// let incr = 9;
+// console.log(++incr);
+// console.log(--decl);
+// let js = +prompt("write number 50", "");
+// // if(js == 50){
+// //     console.log("ok!");
+// // } else if (js > 50){
+// //     console.log("not ok!");
+// // } else {
+// //     console.log("not ok!");
+// // }
+
+// switch (js) {
+//     case 49:
+//     console.log("not ok");
+//     break;
+//     case 51:
+//      console.log("not ok");
+//      break;
+//      case 50:
+//          console.log("ok");
+// }
+// for (let i = 1; js < 55; i++) {
+//     console.log(js);
+//     js++;
+//     if (js == 55){
+//         console.log("ok");
+//     }
+// } 
+
+// let personalMovieDB = {
+//     count: 22,
+// };
+// function calc(va, ga) {
+//     return (va + ga);
+// }
+// console.log(calc(personalMovieDB.count, " movies have been watched"));
+// console.log(calc(5, 6));
+
+// function showSmt(a) {
+//     console.log(a)
+// }
+
+// showSmt("haha");
+// showSmt("hoho");
+// showSmt(9+9);
+
+// let fun = (a, b) => a + b;
+
+// console.log(fun(1,2));
+
+const logg = "Hello World!";
+console.log(logg.slice(6, 11));
